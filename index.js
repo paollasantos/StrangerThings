a6b1864829c749dd8baabfa949f651eaa2af7644
Paola dos Santos Moreira <paollasantos177@gmail.com>
2022-07-30 21:07:30 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function Login () {
+    window.location = "index2.html";
+}
+
+function inscrever() {
+    window.location = "index3.html";
+}
+
+function Cadastrar() {
+    window.location = "index.html"
+}
---
+++ index2.js
@@ -0,0 +1,154 @@
+
+
+function card01(){
+
+    let modal01 = document.querySelector('.modal01') 
+
+
+    modal01.style.display = 'block';
+}
+
+function fecharmodal01() {
+    let modal01 = document.querySelector('.modal01')
+    modal01.style.display = 'none';
+}
+
+    
+
+function card02(){
+
+    let modal02 = document.querySelector('.modal02')
+
+
+    modal02.style.display = 'block';
+}
+
+function fecharmodal02() {
+    let modal02 = document.querySelector('.modal02')
+    modal02.style.display = 'none';
+}
+
+function card03(){
+
+    let modal03 = document.querySelector('.modal03')
+
+
+    modal03.style.display = 'block';
+}
+
+function fecharmodal03() {
+    let modal03 = document.querySelector('.modal03')
+    modal03.style.display = 'none';
+}
+
+function card04(){
+
+    let modal04 = document.querySelector('.modal04')
+
+
+    modal04.style.display = 'block';
+}
+
+function fecharmodal04() {
+    let modal04 = document.querySelector('.modal04')
+    modal04.style.display = 'none';
+}
+
+function card05(){
+
+    let modal05 = document.querySelector('.modal05')
+
+
+    modal05.style.display = 'block';
+}
+function fecharmodal05() {
+    let modal05 = document.querySelector('.modal05')
+    modal05.style.display = 'none';
+}
+
+function card06(){
+
+    let modal06 = document.querySelector('.modal06')
+
+
+    modal06.style.display = 'block';
+}
+
+function fecharmodal06() {
+    let modal06 = document.querySelector('.modal06')
+    modal06.style.display = 'none';
+}
+
+function card07(){
+
+    let modal07 = document.querySelector('.modal07')
+
+
+    modal07.style.display = 'block';
+}
+function fecharmodal07() {
+    let modal07 = document.querySelector('.modal07')
+    modal07.style.display = 'none';
+}
+
+function card08(){
+
+    let modal08 = document.querySelector('.modal08')
+
+
+    modal08.style.display = 'block';
+}
+function fecharmodal08() {
+    let modal08 = document.querySelector('.modal08')
+    modal08.style.display = 'none';
+}
+
+function card09(){
+
+    let modal09 = document.querySelector('.modal09')
+
+
+    modal09.style.display = 'block';
+}
+function fecharmodal09() {
+    let modal09 = document.querySelector('.modal09')
+    modal09.style.display = 'none';
+}
+
+function card010(){
+
+    let modal010 = document.querySelector('.modal010')
+
+
+    modal010.style.display = 'block';
+}
+function fecharmodal010() {
+    let modal010 = document.querySelector('.modal010')
+    modal010.style.display = 'none';
+}
+
+function card011(){
+
+    let modal011 = document.querySelector('.modal011')
+
+
+    modal011.style.display = 'block';
+}
+function fecharmodal011() {
+    let modal011 = document.querySelector('.modal011')
+    modal011.style.display = 'none';
+}
+
+function card012(){
+
+    let modal012 = document.querySelector('.modal012')
+
+
+    modal012.style.display = 'block';
+}
+function fecharmodal012() {
+    let modal012 = document.querySelector('.modal012')
+    modal012.style.display = 'none';
+}
+
+
